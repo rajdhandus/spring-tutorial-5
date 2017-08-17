615b46298af0f170af4d9bbb86d6764adb19c013
Rajkumar Purushothaman <raj.dhandus@gmail.com>
2017-08-16 20:16:07 -0400

    commiting the latest

--- src/main/java/com/rajkumar/spring/Jungle.java
+++ src/main/java/com/rajkumar/spring/Jungle.java
@@ -1,12 +1,23 @@
 package com.rajkumar.spring;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Jungle {
 	
 	private Animal largest;
 	private List<Animal> animals;
+	private Map<String, String> foodsForAnimals = new HashMap<String, String>();
+
 	
+	
+	/**
+	 * @param foodsForAnimals the foodsForAnimals to set
+	 */
+	public void setFoodsForAnimals(Map<String, String> foodsForAnimals) {
+		this.foodsForAnimals = foodsForAnimals;
+	}
 
 	/**
 	 * @return the largest
@@ -41,8 +52,10 @@ public class Jungle {
 	 */
 	@Override
 	public String toString() {
-		return "Jungle [largest=" + largest + ", animals=" + animals + "]";
+		return "Jungle [largest=" + largest + ", animals=" + animals + ", foodsForAnimals=" + foodsForAnimals + "]";
 	}
+
+	
 	
 	
 
